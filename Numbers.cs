f6512efb4fff8dd3f603c2e8cc77362e50fcd479
Rizky Bintang S <38523164+rizkybs70@users.noreply.github.com>
2021-04-01 00:58:36 +0700

    Add files via upload

---
+++ Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace LatihanOverloadingMethod
+{
+    class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            return Math.Min(number1, number2);
+        }
+
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            return (int)(new float[] { number1, number2, number3 }).Min();
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            return Math.Max(number1, number2);
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            return (int)(new float[] { number1, number2, number3 }).Max();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LatihanOverloadingMethod
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Numbers numbers = new Numbers();
+
+			Console.WriteLine("Minimum #1: {0}", numbers.FindMinimum(3, 1));
+			Console.WriteLine("Minimum #2: {0}", numbers.FindMinimum(3, 2, 4));
+			Console.WriteLine();
+			Console.WriteLine("Minimum #1: {0}", numbers.FindMaximum(3, 1));
+			Console.WriteLine("Minimum #2: {0}", numbers.FindMaximum(3, 2, 4));
+
+		}
+	}
+
+}
